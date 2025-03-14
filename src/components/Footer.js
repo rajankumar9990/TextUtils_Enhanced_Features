73164f351dcfc41cf0f40b1ff8b3c50ab3403693
Rajan <guptarajanapex99@gmail.com>
2025-03-14 19:06:56 +0530

    Commited changes in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function Footer() {
+export default function Footer(myStyle) {
     return (
-        <footer className="text-center mt-4 p-3" style={{ backgroundColor: "#f8f9fa", color: "#333" }}>
+        <footer className="text-center mt-4 p-3"  style={myStyle}>
             <p>
                 © {new Date().getFullYear()} TextUtils | All rights reserved.
             </p>
             <p>
-             <a href="https://github.com/rajankumar9990" target="_blank" rel="noopener noreferrer">Rajan Kumar Gupta</a>
+             <a href="https://github.com/rajankumar9990" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}> <strong style={{ color: "black" }}>Rajan Kumar Gupta</strong> </a>
             </p>
         </footer>
     );
